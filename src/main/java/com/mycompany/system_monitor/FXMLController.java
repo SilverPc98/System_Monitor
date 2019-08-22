c6d6d3faf8f57f6456eb7ca31166bb94b4055fcf
SilverPc98 <totherikjanos@gmail.com>
2019-08-22 10:46:19 +0200

    It has been configured

--- src/main/java/com/mycompany/system_monitor/FXMLController.java
+++ src/main/java/com/mycompany/system_monitor/FXMLController.java
@@ -6,6 +6,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import oshi.SystemInfo;
+import oshi.hardware.Baseboard;
+import oshi.hardware.ComputerSystem;
+import org.slf4j.Logger;
 
 public class FXMLController implements Initializable {
     
@@ -15,11 +19,16 @@ public class FXMLController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        SystemInfo si = new SystemInfo();
+        ComputerSystem cs = si.getHardware().getComputerSystem();
+        Baseboard bb = cs.getBaseboard();
+        System.out.println(bb.getManufacturer());
+        label.setText(bb.getManufacturer());
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
 }
