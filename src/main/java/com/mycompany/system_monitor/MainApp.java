0653306311a5f1479e47b8553cc89ce4fa60917a
SilverPc98 <totherikjanos@gmail.com>
2019-08-22 11:05:20 +0200

    Move controller and basic design

--- src/main/java/com/mycompany/system_monitor/MainApp.java
+++ src/main/java/com/mycompany/system_monitor/MainApp.java
@@ -2,20 +2,46 @@ package com.mycompany.system_monitor;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 
 public class MainApp extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(final Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        stage.initStyle(StageStyle.TRANSPARENT);
+        
+        root.setOnMousePressed(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        
+        root.setOnMouseDragged(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        
+        });
+        
+        
         
         Scene scene = new Scene(root);
+        scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
         scene.getStylesheets().add("/styles/Styles.css");
         
         stage.setTitle("JavaFX and Maven");
